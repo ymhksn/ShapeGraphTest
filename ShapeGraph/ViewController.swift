387bc836d52e3911bfb3217565c69b4bbada3212
Yohanes Markus <ymheksan@gmail.com>
2020-04-07 09:07:38 +0700

    Initial Commit

---
+++ ShapeGraph/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShapeGraph
+//
+//  Created by Yohanes Markus Heksan on 07/04/20.
+//  Copyright © 2020 Yohanes Markus Heksan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
